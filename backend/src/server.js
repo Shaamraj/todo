f6a5b109201a3e5d9d97167d55f957c357564451
shaamraj <shaamrajravikumar@gmail.com>
2026-05-26 18:01:30 +0530

    updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,25 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
 
-const app = require("./app"); // ✅ THIS IS THE FIX
+const mongoose = require("mongoose");
+const app = require("./app");
 
 const startServer = async () => {
   try {
+
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log("DB Connected");
 
-    app.listen(5000, () => {
-      console.log("Server running");
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
 
   } catch (err) {
+
     console.log("DB Error:", err);
+
   }
 };
 
