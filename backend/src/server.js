6247014bc13f3cdb6020d916022e0fb192b3a072
shaamraj <shaamrajravikumar@gmail.com>
2026-06-12 18:09:30 +0530

    updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,19 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const app = require("./app");
 require("./cron/reminderCron");
+const sendEmail = require("./utils/sendEmail");
 const startServer = async () => {
   try {
 
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("DB Connected");
+   
+    await sendEmail(
+        "youractualemail@gmail.com",
+        "Server Started",
+        "TaskFlow email system is working"
+      );
 
     const PORT = process.env.PORT || 5000;
 
