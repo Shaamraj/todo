6266d4bd212f145d452993c032b0b62a0c29961d
shaamraj <shaamrajravikumar@gmail.com>
2026-06-16 19:25:27 +0530

    updated

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,5 +1,7 @@
 const nodemailer = require("nodemailer");
 
+console.log("BREVO_USER =", process.env.BREVO_USER);
+console.log("BREVO_PASS exists =", !!process.env.BREVO_PASS);
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
